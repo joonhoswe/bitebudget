d6cdcfb2ba5e764cf26f61269e6b903b496f5cc8
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2025-02-08 14:04:35 -0500

    update layout paths

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,10 +31,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/signup" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="+not-found" /> */}
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
